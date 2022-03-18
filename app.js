800ab37c3f5b4c6baabecf7b1c2e931c770e5044
Sujan Gamal <invadorshadow@gmail.com>
2022-03-18 11:13:18 +0545

    pages folder added

--- app.js
+++ app.js
@@ -15,3 +15,49 @@ avtar.addEventListener("click", function(){
     
     dashboardContainer.classList.toggle("show-dashboard-container");
 })
+
+// ===============================================================================================
+// View Estimation 
+// chart detail
+// ===============================================================================================
+
+const labels = [
+    'Task A',
+    'Task B',
+    'Task c',
+    'Task D',
+    'Task E'
+   
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [
+          [10,18], [5,10], [8,12], [5, 10], [2,6]
+        //   18, 10, 5, 2, 20, 30, 45
+        ],
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+
+        plugins: {
+          title: {
+            display: true,
+            text: 'Chart Title',
+          }
+        }
+      }
+    };
+    
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
